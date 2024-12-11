6eb36c1634a534c9faddb78d4e5139a9f84dafc0
DevHDL <whiper403@naver.com>
2024-12-12 02:41:56 +0900

    Edit Base/01_Perceptron - perceptron -> study_perceptron.ipynb - create example dir - Edit example/Perceptron.py     - weight -> weight, bias     - input_num -> units - create example/ex_perecptron.ipynb     - use my custom Class example/Perceptron.py

--- Base/01_Perceptron/example/Perceptron.py
+++ Base/01_Perceptron/example/Perceptron.py
@@ -3,48 +3,51 @@ import numpy as np
 class Perceptron:
     def __init__(
             self, 
-            input_num:int=None,
+            units:int=None,
             lr:float=0.01
         ):
-        self.input_num:int = input_num
+        '''Perceptron
+        parameters
+        - units : number of inputs(int)
+        - lr : learning_rate(float)
+        ''' 
+        self.units:int = units
         self.learning_rate:float = lr
         self.weight:np.ndarray = None
+        self.bias:np.ndarray = np.random.uniform(-1, 1, size=1)
 
-        if input_num != None:
-            self.weight:np.ndarray = np.random.uniform(-1, 1, size=self.input_num+1)
+        if units != None:
+            self.weight:np.ndarray = np.random.uniform(-1, 1, size=self.units)
 
-    def Input(self, input_num:int=None):
-        if self.input_num == None:
-            self.input_num = input_num
+    def Input(self, units:int=None):
+        if self.units == None:
+            self.units = units
         
-        self.weight = np.random.uniform(-1, 1, size=self.input_num+1)
+        self.weight = np.random.uniform(-1, 1, size=self.units)
         
     def Activation(self, X):
         # step function
         return np.where(X > 0, 1, 0)
 
     def Forward(self, x):
-        X = np.dot(x, self.weight[1:]) + self.weight[0]
+        X = np.dot(x, self.weight) + self.bias
         y = self.Activation(X)
         return y
     
     def Back(self, update, x):
-        self.weight[1:] += update * x
-        self.weight[0] += update
+        self.weight += update * x
+        self.bias += update
     
     def Train(self, 
             X:np.ndarray, 
             y:np.ndarray,
             epochs:int
         ):
-        if X.shape != self.weight.shape: return False
-
         for epoch in range(epochs):
             for x_i, y_i in zip(X,y):
                 predict_y = self.Forward(x_i)
                 update = self.learning_rate * (y_i - predict_y)
                 self.Back(update, x_i)
-        return True
     
     def Predict(self, X):
         return self.Forward(X)
